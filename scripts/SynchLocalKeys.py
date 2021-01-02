ef0cd0c305d930bda24a2d3b7ea1eb2e08fb8856
POGVaz <pedro_o99@hotmail.com>
2021-01-02 14:07:10 -0300

    Synch Localisation use all files in dir

--- scripts/SynchLocalKeys.py
+++ scripts/SynchLocalKeys.py
@@ -1,34 +1,15 @@
 # Synchronizes new keys in English to other languages while keeping the previous keys untouched.
 
-#import os
+from pathlib import Path
 
 ref_language = 'english'
-languages = [
-    "braz_por",
-    "french",
-    "german",
-    "polish",
-    "russian",
-    "simp_chinese",
-    "spanish",
-]
+LOCAL_DIR = Path('..') / 'localisation'
 
-file_location = "./../localisation/"
-file_extension = ".yml"
-files = [
-    "pw_ambitions_l_",
-    "pw_armies_l_",
-    "pw_ascension_perks_l_",
-    "pw_buildings_l_",
-    "pw_decisions_l_",
-    "pw_messages_l_",
-    "pw_policies_l_"
-    "pw_pop_jobs_l_",
-    "pw_solipsist_debate_events_"
-    "pw_static_modifiers_l_",
-    "pw_tech_l_",
-    "pw_wonder_events_l_",
-]
+#Get all reference localisation files:
+files = list(LOCAL_DIR.glob(ref_language+'/*.yml'))
+
+#Get all languages directories:
+languages = [x.name for x in LOCAL_DIR.iterdir() if x.is_dir() and ref_language not in x.name]
 
 #Test settings:
 # languages = ["other"]
@@ -36,7 +17,7 @@ files = [
 # file_extension = ".yml"
 # files = ['test_1_l_', 'test_2_l_']
 
-def getLocalisationKeys(ref_file_path):
+def getLocalisationKeys(ref_file):
     '''
         Returns a dictionary with the localisation
         keys as keys and the localisation
@@ -45,30 +26,30 @@ def getLocalisationKeys(ref_file_path):
     local_map = {}
 
     try:
-        with open(ref_file_path) as ref_file:
-            next(ref_file) #Skips first line
-            for line in ref_file:
+        with ref_file.open() as file:
+            next(file) #Skips first line
+            for line in file:
                 line = line.lstrip() #Clears whitespaces
-                if line and not line.startswith('#'):
-                    split_line = line.split(':', 1)
+                if line and not line.startswith('#'): #Ignores comments
+                    split_line = line.split(':', 1) #Separate keys and text
                     local_map[split_line[0]] = split_line[1]
     except FileNotFoundError:
-        print("File Not Found: ", ref_file_path)
+        print("File Not Found: ", ref_file.name)
         raise
 
     return local_map
 
-def appendLocalisationKeys(file_path, localisationMap):
+def appendLocalisationKeys(local_file, localisationMap):
     '''
         Appends a dictionary with the localisation
         keys and content to a file.
     '''
 
-    with open(file_path, 'a', encoding='utf-8') as file:
+    with local_file.open('a', encoding='utf-8') as file:
         file.write('\n')
         for key in localisationMap:
             file.write('    {0}:{1}'.format(key, localisationMap[key]))
-            print("Added {0} key to {1}".format(key, file_path))
+            print("Added {0} key to {1}".format(key, local_file.name))
 
 def getDiffDict(ref_dict, comp_dict):
     '''
@@ -87,30 +68,30 @@ def getDiffDict(ref_dict, comp_dict):
     return result_dict
 
 
-def createNewLanguageFile(file_path, language):
+def createNewLanguageFile(new_file, language):
     '''
         Creates a new file in the specified path with the header:
         l_[language passed]:
     '''
-    print("Creating new file: ", file_path)
-    with open(file_path, 'w', encoding='utf-8') as file:
+    print("Creating new file: ", new_file)
+    with new_file.open('w', encoding='utf-8') as file:
         file.write('l_{0}:\n\n    '.format(language))
 
 for file in files:
     try:
-        ref_keys = getLocalisationKeys(
-            file_location + ref_language + '/' + file + ref_language + file_extension)
+        ref_keys = getLocalisationKeys(file)
     except:
-        print("Skipping file: ", file_location + ref_language +
-              '/' + file + ref_language + file_extension)
+        print("Skipping file: ", file.name)
         continue
     for language in languages:
-        file_path = file_location + language + '/' + file + language + file_extension
+        language_file = LOCAL_DIR / language / file.name.replace(ref_language, language)
         try:
-            current_keys = getLocalisationKeys(file_path)
+            current_keys = getLocalisationKeys(language_file)
         except FileNotFoundError: #No file for the current language
-            createNewLanguageFile(file_path, language)
+            createNewLanguageFile(language_file, language)
             current_keys = {}
+
+
         missing_keys = getDiffDict(ref_keys, current_keys)
         if (missing_keys):
-            appendLocalisationKeys(file_path, missing_keys)
+            appendLocalisationKeys(language_file, missing_keys)
